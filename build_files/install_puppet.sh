a50bd1b6a29a4009e9cfe87e8e4400bf9018c318
Jordan@rimmer <jordan.likes.curry@gmail.com>
2025-09-24 07:46:10 -0700

    Fail to install puppet for some reason 😢

--- build_files/install_puppet.sh
+++ build_files/install_puppet.sh
@@ -7,4 +7,5 @@ os_name="fedora"
 version="42"
 curl -o openvox.rpm "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${version}.noarch.rpm"
 dnf5 install -y ./openvox.rpm
+#dnf5 --installroot=/var --setopt=persistdir=$(mktemp -d) --use-host-config install -y openvox-agent
 dnf5 install -y openvox-agent
