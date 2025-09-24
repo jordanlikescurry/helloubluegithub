c13dc6f4b5110c634fc71dc3c7248f752e84f271
Jordan@rimmer <jordan.likes.curry@gmail.com>
2025-09-23 23:13:19 -0700

    Fix not actually installing openvox agent after installing release

--- build_files/install_puppet.sh
+++ build_files/install_puppet.sh
@@ -7,3 +7,4 @@ os_name="fedora"
 version="42"
 curl -o openvox.rpm "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${version}.noarch.rpm"
 dnf5 install -y ./openvox.rpm
+dnf5 install -y openvox-agent
