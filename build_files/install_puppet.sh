812baff77ca622f54c765058a4f7e2554f6dad3b
Jordan@rimmer <jordan.likes.curry@gmail.com>
2025-09-28 10:46:53 -0700

    Try and fail to install puppet with rpm-ostree

--- build_files/install_puppet.sh
+++ build_files/install_puppet.sh
@@ -2,10 +2,14 @@
 
 set -ouex pipefail
 
-release="8"
-os_name="fedora"
-version="42"
-curl -o openvox.rpm "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${version}.noarch.rpm"
-dnf4 install -y ./openvox.rpm
+source /etc/os-release
+openvox_release="8"
+os_name="${ID}"
+version="${VERSION_ID}"
+curl -o openvoxrelease.rpm "https://yum.voxpupuli.org/openvox${openvox_release}-release-${os_name}-${version}.noarch.rpm"
+dnf5 install -y ./openvoxrelease.rpm
+rm ./openvoxrelease.rpm
+# It seems like we can't install openvox agent any which way during the container build stage
 #dnf5 --installroot=/var --setopt=persistdir=$(mktemp -d) --use-host-config install -y openvox-agent
 #dnf5 install -y openvox-agent
+#rpm-ostree install -y openvox-agent
